d2b77fa67d81203ed55c47882628b3639345788a
Nathan Reidy <nathanjreidy@gmail.com>
2021-04-26 17:51:48 +1000

    Added state for plus minus btm and added conditional rendering

--- src/Question.js
+++ src/Question.js
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 const Question = ({ id, title, info }) => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="questionContainer">
       <div className="topContainer">
         <h4 className="question">{title}</h4>
-        <div className="plusBtn">
-          <AiOutlinePlus />
-        </div>
+
+        {open ? (
+          <div className="minusBtn" onClick={() => setOpen(!open)}>
+            <AiOutlineMinus />
+          </div>
+        ) : (
+          <div className="plusBtn" onClick={() => setOpen(!open)}>
+            <AiOutlinePlus />
+          </div>
+        )}
       </div>
-      <p className="infoContainer">{info}</p>
+      {open ? <p className="infoContainer">{info}</p> : null}
     </div>
   );
 };
